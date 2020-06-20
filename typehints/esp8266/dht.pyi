0517e063a7d50696ea310e3b00b21e26e188aa7c
w0nd4bra <w0nd4bra@users.noreply.github.com>
2020-06-20 11:59:05 +0200

    Create dht.pyi

---
+++ typehints/esp8266/dht.pyi
@@ -0,0 +1,50 @@
+"""DHT driver
+
+The DHT driver is implemented in software and works on all pins:
+
+    import dht
+    import machine
+
+    d = dht.DHT11(machine.Pin(4))
+    d.measure()
+    d.temperature() # eg. 23 (°C)
+    d.humidity()    # eg. 41 (% RH)
+
+    d = dht.DHT22(machine.Pin(4))
+    d.measure()
+    d.temperature() # eg. 23.6 (°C)
+    d.humidity()    # eg. 41.3 (% RH)
+
+"""
+
+
+class DHT11:
+    ''
+    def humidity(self) -> int:
+        ...
+
+    def measure(self):
+        ...
+
+    def temperature(self) -> int:
+        ...
+
+
+class DHT22:
+    ''
+    def humidity(self) -> float:
+        ...
+
+    def measure(self):
+        ...
+
+    def temperature(self) -> float:
+        ...
+
+
+class DHTBase:
+    ''
+    def measure(self):
+        ...
+
+
