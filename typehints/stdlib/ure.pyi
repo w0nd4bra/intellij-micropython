428030b040d8ae8e9e5a5801ea10b388245310e7
w0nd4bra <w0nd4bra@users.noreply.github.com>
2020-10-04 20:08:46 +0200

    Create ure.pyi

---
+++ typehints/stdlib/ure.pyi
@@ -0,0 +1,22 @@
+"""ure – simple regular expressions
+
+This module implements a subset of the corresponding CPython module, as described below. 
+For more information, refer to the original CPython documentation: re.
+
+This module implements regular expression operations. 
+Regular expression syntax supported is a subset of CPython re module (and actually is a subset of POSIX extended regular expressions).
+"""
+
+
+def compile():
+    ...
+
+def match():
+    ...
+
+def search():
+    ...
+
+def sub():
+    ...
+
